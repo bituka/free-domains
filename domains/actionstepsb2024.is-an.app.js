ead82003590fe50c84a582fafd3f4a2314fcd6a5
bituka <goryo.webdev@gmail.com>
2024-05-08 18:44:54 +0800

    Create actionstepsb2024.is-an.app.js

---
+++ domains/actionstepsb2024.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+    description: 'A NetSuite sandbox for My Account testing',
+    domain: 'is-an.app',
+    subdomain: 'actionstepsb2024',
+    owner: {
+      email: 'goryo.webdev@gmail.com',
+    },
+    record: {
+      CNAME: 'actionstepsb2024.is-an.app.hosting.netsuite.com'
+    },
+    proxy: false
+  })
